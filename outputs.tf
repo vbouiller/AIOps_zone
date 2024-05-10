21d66a7efdf3f92b31fe39f998189844904584a6
VBouiller <31282661+vbouiller@users.noreply.github.com>
2024-05-10 10:20:09 +0200

    fix: cleaned up some code and added output (#2)
    * fix: cleaned up some code and added output
    
    * fix: fixed output reference in outputs

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,10 @@ output "app_public_ip" {
   value = azurerm_linux_virtual_machine.app.public_ip_address
 }
 
+output "app_URL" {
+  value = "http://${azurerm_linux_virtual_machine.app.public_ip_address}:5000"
+}
+
 output "app_username" {
   value = azurerm_linux_virtual_machine.app.admin_username
 }
