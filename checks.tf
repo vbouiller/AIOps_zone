b73a97bbac679112acb4f2ca751795253a4820dd
Valentin Bouiller <31282661+vbouiller@users.noreply.github.com>
2024-06-03 12:45:13 +0200

    fix: typo and format

--- checks.tf
+++ checks.tf
@@ -1,10 +1,10 @@
 check "front_end_200" {
-    data "htt" "front_end" {
-        url = output.app_URL.value
-    }
+  data "http" "front_end" {
+    url = output.app_URL.value
+  }
 
-    assert {
-      condition = data.http.front_end.status_code == 200
-      error_message = "${data.http.front_end.url} a retourné un mauvais code de statut: ${data.http.front_end.status_code}"
-    }
+  assert {
+    condition     = data.http.front_end.status_code == 200
+    error_message = "${data.http.front_end.url} a retourné un mauvais code de statut: ${data.http.front_end.status_code}"
+  }
 }
