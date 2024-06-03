ba9a3f69db318f4188a8de57cfbd1ed1162c52b5
Valentin Bouiller <31282661+vbouiller@users.noreply.github.com>
2024-06-03 12:43:43 +0200

    feat: added check on front_end status

---
+++ checks.tf
@@ -0,0 +1,10 @@
+check "front_end_200" {
+    data "htt" "front_end" {
+        url = output.app_URL.value
+    }
+
+    assert {
+      condition = data.http.front_end.status_code == 200
+      error_message = "${data.http.front_end.url} a retourné un mauvais code de statut: ${data.http.front_end.status_code}"
+    }
+}
